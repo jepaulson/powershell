b455ae1a5cf51b92d0c68053250b0e3ff2cfa732
jepaulson <48519486+jepaulson@users.noreply.github.com>
2019-08-27 15:11:04 -0500

    Add files via upload

---
+++ rdpregfix.ps1
@@ -0,0 +1,3 @@
+﻿$regpath = 'Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa\Kerberos\Parameters'
+
+New-ItemProperty $regpath -Name "MaxTokenSize" -PropertyType "DWORD" -Value "48000"
